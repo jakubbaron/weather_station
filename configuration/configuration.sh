619e50ad128964f7bc4fa3ce572a36ded33048cd
Jakub Baron <jakubaron@gmail.com>
2018-12-21 00:15:19 +0100

    Adding Adafruit_Python_DHT dependency

--- configuration/configuration.sh
+++ configuration/configuration.sh
@@ -2,3 +2,7 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install vim
 sudo apt-get install redis-tools
+# https://github.com/adafruit/Adafruit_Python_DHT
+sudo apt-get install python3-pip
+sudo python3 -m pip install --upgrade pip setuptools wheel
+sudo pip3 install Adafruit_DHT
