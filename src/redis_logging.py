126df939a2bb5533cd0029b0b7ad6795e442cd9e
Jakub Baron Rasp <jakubaron@gmail.com>
2018-12-22 21:36:33 +0000

    Playing around with the streams, being able to add mock data from temperature and humidity sensor with a date to a stream, then tested it with redis-cli, it was all visible as expected

--- src/redis_logging.py
+++ src/redis_logging.py
@@ -1,21 +1,34 @@
 import os
 import sys
+import time
 import redis
+
+from collections import namedtuple
+Stats = namedtuple("Stats", ["average", "min", "max"])
 
 def get_redis_passwd():
     try:
         redis_passwd = os.environ['REDIS_PASSWD']
     except KeyError:
-        print "Please provide REDIS_PASSWD to connect to redis"
+        print("Please provide REDIS_PASSWD to connect to redis")
         sys.exit(1)
 
     return redis_passwd
+
+def get_stats(measurements):
+    return Stats(average=sum(measurements) / float(len(measurements)),
+                 min=min(measurements),
+                 max=max(measurements))
 
 def main():
 
     r=redis.StrictRedis(host='localhost', port=6379, db=0, password=get_redis_passwd(), socket_timeout=None, connection_pool=None, charset='utf-8', errors='strict', unix_socket_path=None)
 
-    print r.get('cat')
+    stream="DHT11-stream"
+    humidities=[30,30,30,31,32]
+    temperatures=[24,25,24,24,23]
+    message_template = "{th.average}, {th.min}, {th.max}"
+    print(r.xadd(stream, {'date': time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()), 'temperature': message_template.format(th=get_stats(temperatures)), 'humidites': message_template.format(th=get_stats(humidities))}, maxlen=150000))
 
 if __name__ == "__main__":
     main()
