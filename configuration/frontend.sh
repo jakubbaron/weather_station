d4ea97a6ccd6c65542809fd146b5e3a270be2ae2
Jakub Baron Rasp <jakubaron@gmail.com>
2018-12-24 20:41:48 +0000

    Adding reactjs to the project with the nodejs and react configuration

---
+++ configuration/frontend.sh
@@ -0,0 +1,7 @@
+wget https://nodejs.org/dist/v10.14.2/node-v10.14.2-linux-armv7l.tar.xz
+tar -xFj node-v10.14.2-linux-armv71.tar.xz
+cd node-v10.14.2-linux-armv7l/
+sudo cp -R * /usr/local/
+node -v
+npm -v
+#npx create-react-app frontend
