7c7800e5eff650fb90f1828a320eeb2fa2fee69b
Jakub Baron Rasp <jakubaron@gmail.com>
2018-12-25 23:33:53 +0000

    Adding websocket receiver to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,16 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+var ws = new WebSocket("ws://127.0.0.1:5678/"),
+    messages = document.createElement('ul');
+ws.onmessage = function (event) {
+    var messages = document.getElementsByTagName('ul')[0],
+        message = document.createElement('li'),
+        content = document.createTextNode(event.data);
+    message.appendChild(content);
+    messages.appendChild(message);
+};
+document.body.appendChild(messages);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
